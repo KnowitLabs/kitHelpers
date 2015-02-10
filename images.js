62d965fc6490da15a450c6933bededd4d37ff9dd
Andreas Norman <andreas.norman@knowit.se>
2015-02-10 10:06:22 +0100

    no message

--- images.js
+++ images.js
@@ -1,19 +1,24 @@
-function getRandomImageUrl(type) {
-  var image = '';
-  var item = Math.floor(Math.random() * 7) + 1;
-  if (type === 'rectangle') {
-    image = 'rectangle{X}.png'.replace('{X}', item);
-  } else if (type === 'square') {
-    image = 'square{X}.png'.replace('{X}', item);
-  } else if (type === 'slide') {
-    image = 'slide{X}.png'.replace('{X}', item);
-  } else if (type === 'person') {
-    image = 'person{X}.png'.replace('{X}', item);
-  }
-  return image;
-}
+(function() {
+  module.exports.register = function(Handlebars, options) {
 
-Handlebars.registerHelper('image', function (type, block) {
-  var image = getRandomImageUrl(type);
-  return image;
-});
+    function getRandomImageUrl(type) {
+      var image = '';
+      var item = Math.floor(Math.random() * 7) + 1;
+      if (type === 'rectangle') {
+        image = 'rectangle{X}.png'.replace('{X}', item);
+      } else if (type === 'square') {
+        image = 'square{X}.png'.replace('{X}', item);
+      } else if (type === 'slide') {
+        image = 'slide{X}.png'.replace('{X}', item);
+      } else if (type === 'person') {
+        image = 'person{X}.png'.replace('{X}', item);
+      }
+      return image;
+    }
+
+    Handlebars.registerHelper('image', function (type, block) {
+      var image = getRandomImageUrl(type);
+      return image;
+    });
+  };
+}).call(this);
